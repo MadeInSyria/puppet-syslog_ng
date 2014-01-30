ea408232310df956c5d647f5cb66630bae3faba7
Omar Alaouf <omar@alaouf.com>
2014-01-30 14:16:57 -0800

    Adding file overriding for hostname and environement specific configuration

--- manifests/client.pp
+++ manifests/client.pp
@@ -10,7 +10,9 @@ class syslog_ng::client inherits syslog_ng {
     file { 
         "/etc/syslog-ng/syslog-ng.conf":
             notify  => Service["syslog-ng"],
-            content => template("syslog_ng/syslog-ng.conf.client.erb");
+            content => [template("syslog_ng/syslog-ng.conf.client.$hostname.erb"),
+                        template("syslog_ng/syslog-ng.conf.client.$environement.erb"),
+                        template("syslog_ng/syslog-ng.conf.client.erb")];
         "/etc/logrotate.d/syslog":
             source => "puppet:///modules/syslog_ng/syslog-logrotate";
     } # file
